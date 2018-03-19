2907208a8177ae3db23f5029d29e5086811713f6
Samuel Cruz <samuel.cruz.dev@gmail.com>
2018-03-19 21:00:55 +0100

    Added Twitter Bootstrap css script / dynamic table with data (jQuery)

--- index.php
+++ index.php
@@ -1,46 +1,90 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+  <title>ISS Location Tracker</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script async defer
   src="https://maps.googleapis.com/maps/api/js?key=[YOUR_API_KEY]">
 </script>
 <style>
- #map {
+#map {
   height: 400px;
-  width: 50%;
 }
+
+th, td {
+  text-align: center;
+}
+
+.image {
+    -webkit-animation:spin 4s linear infinite;
+    -moz-animation:spin 4s linear infinite;
+    animation:spin 4s linear infinite;
+}
+@-moz-keyframes spin { 100% { -moz-transform: rotate(360deg); } }
+@-webkit-keyframes spin { 100% { -webkit-transform: rotate(360deg); } }
+@keyframes spin { 100% { -webkit-transform: rotate(360deg); transform:rotate(360deg); } }
+
 </style>
 </head>
 <body>
-  <h3>Current ISS Location</h3>
-  <label for="latitude">Latitude</label>
-  <input id="latitude" type="text" disabled></div>
-  <label for="longitude">Longitude</label>
-  <input id="longitude" type="text" disabled></div>
-  <div id="loading"></div>
-  <div id="map" style="margin-top:10px"></div>
+  
+  <div class="container-fluid">
+    <h3>Current ISS Location</h3>
+    <div class="row">
+      <div class="col-md-6">
+        <label for="latitude">Latitude</label>
+        <input id="latitude" type="text" disabled>
+        <label for="longitude">Longitude</label>
+        <input id="longitude" type="text" disabled>
+        <div id="loading" style="float:right"></div>
+        <div id="map" style="margin-top:10px"></div>
+      </div>
+      <div class="col-md-6">
+
+       <table id="locationData" width="100%">
+        <tr>
+          <tbody>
+            <th style="display:none">Latitude</th>
+            <th style="display:none">Longitude</th>
+            <tr>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </tr>
+
+      </table>
+    </div>
+  </div>
   <script>
 
     var calledMap = false;
-      $(window).on('load', function(){ 
-          initMap();
-          calledMap = true;
+    $(window).on('load', function(){ 
+      initMap();
+        //  $('th').css("display","none");
+        calledMap = true;
 
-          if (calledMap){
-            setInterval(function(){
-              initMap();
-            },5000);
-          }
-        });
-      
-     function initMap() {
+        if (calledMap){
+          setInterval(function(){
+            initMap();
+            $('th').show();
+          },5000);
+
+
+        }
+
+      });
+
+    function initMap() {
       $(document).ready(function() {
         var valores = $.ajax({
           type: "GET", 
           url: "iss_data.php", 
           beforeSend: function() {
-            $('#loading').text('Cargando...');
+            $('#loading').html('<img class="image" src="img/updating.png" />');
           },
           complete: function() {
             $('#loading').text('');
@@ -60,9 +104,15 @@
         });
       });
 
+      $('#locationData > tbody:last-child').append(
+        '<tr>'
+        +'<td>' + $('#latitude').val() +  '</td>'
+        +'<td>' + $('#longitude').val() +  '</td>'
+        +'</tr>');     
     }
 
- </script>
+
+  </script>
 
 </body>
 </html>
--- readme.md
+++ readme.md
@@ -6,4 +6,4 @@ You have to add your Google API Key into [YOUR_API_KEY] tag included in code.
 ` <script async defer
   src="https://maps.googleapis.com/maps/api/js?key=[YOUR_API_KEY]">`
   
-  ![Map screenshoot](https://github.com/kzblau/iss-location-tracker/blob/master/img/map_screen.jpeg)
+  ![Map screenshoot](https://github.com/kzblau/iss-location-tracker/blob/master/img/map_screen.png)
